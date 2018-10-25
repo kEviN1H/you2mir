c8e5f74de29e5719bcdd2174c6aaf229a4788879
kEviN1H <kEviN1H@users.noreply.github.com>
2018-10-25 16:40:25 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+define('ROOT_PART', Root_part());
+define('APIKEY', 'AIzaSyBh3y-15gwFwsSFgu9AuvTq89LtLcIMwLw');//这里改成您的api key
+define('GJ_CODE', 'CN');//国家代码
+define('SITE_NAME', 'YoutubeMirror');//网站名字
+define('TITLENAME', 'YoutubeMirror');//网站名字
+define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
+define('EMAIL', 'hby0410@hotmail.com');//邮箱
+?>
